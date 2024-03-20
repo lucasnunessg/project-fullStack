5ea931ebb55e77dfd5e88094ec99b92d0b3aecbc
LucasNunesDEV <lucasnunespacheco@gmail.com>
2024-03-20 17:04:34 -0300

    só falta o delete

--- back-end/src/controller/user.controller.js
+++ back-end/src/controller/user.controller.js
@@ -25,7 +25,35 @@ const getById = async (req, res) => {
     }
   };
 
+  const createUser = async (req, res) => {
+    try {
+        const { full_name, email, contact } = req.body;
+        const newUser = await userService.createUser(full_name, email, contact);
+        return res.status(201).json(newUser);
+    } catch(e){
+        console.log(e.message)
+        res.status(500).json({ message: error500Message });
+    }
+  };
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { full_name, email, contact } = req.body;
+
+        const updateUser = await userService.updateUser(id, full_name, email, contact);
+        
+        if (!updateUser) return res.status(404).json({ message: 'Não foi possível atualizar os dados' })
+        return res.status(200).json({ message: 'Atualizado com sucesso!' });
+    } catch(e) {
+        console.log(e.message);
+        res.status(500).json({ message: error500Message })
+    }
+}
+
 module.exports = {
     getAll,
     getById,
+    createUser,
+    updateUser,
 }
--- back-end/src/services/user.service.js
+++ back-end/src/services/user.service.js
@@ -10,9 +10,26 @@ const getById = async (id) => {
     const users = await User.findByPk(id);
 
     return users;
+};
+
+const createUser = async (full_name, email, contact) => {
+    const users = await User.create({full_name, email, contact});
+
+    return users;
+}
+
+const updateUser = async (id, full_name, email, contact) => {
+    const [updateUser] = await User.update(
+        {full_name, email, contact},
+        { where: { id } },
+    );
+    
+    return updateUser;
 }
 
 module.exports = {
     getAll,
     getById,
+    createUser,
+    updateUser,
 }
